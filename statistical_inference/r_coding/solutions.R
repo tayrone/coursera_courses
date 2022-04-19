0de64f0290cf51449b16b66e1285110fd784f9ba
Tayrone <tayronesm@gmail.com>
2022-04-19 15:53:50 -0300

    finished this swirl course

---
+++ statistical_inference/r_coding/solutions.R
@@ -0,0 +1,34 @@
+# Lesson: Expectations
+
+library(swirl)
+swirl()
+
+Tayrone
+
+1
+5
+
+3.5
+expect_dice
+dice_high
+expect_dice(dice_high)
+expect_dice(dice_low)
+
+(edh+edl)/2
+1
+
+3
+integrate(myfunc, 0, 2)
+spop
+mean(spop)
+allsam
+apply(allsam, 1, mean)
+mean(smeans)
+
+2
+1
+2
+1
+1
+2
+
